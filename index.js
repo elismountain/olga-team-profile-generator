3bbfa26f14ffc836dde97a794f9c37881f5a3f20
elismountain <echospb@mail.ru>
2024-02-09 23:01:40 +0000

    added create manager function

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -12,4 +15,76 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+const teamArray = [];
+
+
+const createManager = () => {
+    console.log('Please, build your team');
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "Who is the menager on this team?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please, enter the manager name');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter the manager's ID.",
+            validate: idInput => {
+                if(isNaN(idInput)) {
+                    console.log('Please, enter the manager ID');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the manager's email.",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email!')
+                    return false; 
+                }
+            }
+        }, 
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Please enter the manager's office number",
+            validate: officeInput => {
+                if(isNaN(officeInput)) {
+                    console.log('Please, enter the office number');
+                    return false;
+                } else {
+                    return true;
+                }
+            } 
+        }, 
+    ])
+    .then(managerInput => {
+        const  { name, id, email, officeNumber } = managerInput; 
+        const manager = new Manager (name, id, email, officeNumber);
+
+        teamArray.push(manager); 
+        console.log(manager); 
+    })
+}
+
+
+createManager();
 
